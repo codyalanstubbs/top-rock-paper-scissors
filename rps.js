6c99e933186dc31403c6b0c5f0b0a7fd942bd930
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-21 09:51:10 -0400

    Make plan for first four steps of exercise

---
+++ rps.js
@@ -0,0 +1,10 @@
+// No input
+// Randomly select 'Rock', 'Paper', or 'Scissors'
+// Return result
+
+// Two parameters: playerSelection & computerSelection
+// Run only a single round of the game
+// Process parameters to make case insensitive, e.g.: both to lowercase
+// Compare the two parameters to determine winner
+// If player wins, return "You Lose! ${playerSelection} beats ${computerSelection}"
+// if computer wins, return "You Lose! ${computerSelection} beats ${playerSelection}"
