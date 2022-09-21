18eb4c9c2ceec5639b7ca22aa48c90c829921e99
codyastubbs <5488363+codyastubbs@users.noreply.github.com>
2022-09-21 10:19:49 -0400

    Create playRound function
    The function takes the playerSelection and computerSelection
    parameters, makes both selections lowercase, and compares them
    to return a result of a single round. The result will either
    declare a tie or a win or loss for the user. Currently the user
    input is hardcoded so a prompt will need to be added.

--- rps.js
+++ rps.js
@@ -9,7 +9,27 @@ function getComputerChoice() {
 
 // Two parameters: playerSelection & computerSelection
 // Run only a single round of the game
-// Process parameters to make case insensitive, e.g.: both to lowercase
-// Compare the two parameters to determine winner
-// If player wins, return "You Lose! ${playerSelection} beats ${computerSelection}"
-// if computer wins, return "You Lose! ${computerSelection} beats ${playerSelection}"
+function playRound(playerSelection, computerSelection) {
+    
+    // Process parameters to make case insensitive, e.g.: both to lowercase
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    // Compare the two parameters to determine winner
+    if (playerSelection === computerSelection) {
+        return `Tie!`;
+    } else if ( // If player wins, return "You Win! ${playerSelection} beats ${computerSelection}"
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else { // if computer wins, return "You Lose! ${computerSelection} beats ${playerSelection}"
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } 
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
